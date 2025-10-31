f90e0e8498df98d89d822b82b98cfbb4aa09157a
houssam largou <hossamlargo5@gmail.com>
2025-10-31 15:43:07 +0100

    Added word count selection feature in the header section

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const words =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptatem, odio perspiciatis animi recusandae debitis mollitia, ex culpa qui natus id asperiores esse hic sequi? Architecto consequatur eveniet quam illum! ";
 
+let num;
+document.querySelector(".num").addEventListener("click", function (e) {
+  num = e.target.value;
+});
+
 const wordsSplit = words.split(" ");
-console.log(wordsSplit)
 const wordsCount = wordsSplit.length;
-console.log(wordsCount)
 function randomWords() {
   const randomIndex = Math.floor(Math.random() * wordsCount);
   return wordsSplit[randomIndex];
 }
 
 function formatWord(word) {
-    return `<div class="word">${word}<div>`;
+  return `<div class="word">${word}<div>`;
 }
 
 function newGame() {
   document.querySelector(".words").innerHTML = "";
-  for (let i = 0; i < wordsCount; i++) {
+  for (let i = 0; i < num; i++) {
     document.querySelector(".words").innerHTML += formatWord(randomWords());
   }
 }
